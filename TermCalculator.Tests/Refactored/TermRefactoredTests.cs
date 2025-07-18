79f117482813c3b784303430ef330104f5265212
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-07-18 17:01:05 +0200

    Module 006 xUnit Tests

---
+++ TermCalculator.Tests/Refactored/TermRefactoredTests.cs
@@ -0,0 +1,76 @@
+﻿using TermCalculator.Refactored;
+
+namespace TermCalculator.Tests.Refactored;
+
+public class TermRefactoredTests
+{
+    // Good Cases
+    [Fact]
+    public void Term_ValidInputs_ShouldParseCorrectly()
+    {
+        // Arrange -- Ausgangszustand des Tests vorbereiten
+        var term = new Term("25+13");
+
+        // Act -- Test ausführen
+        term.Parse();
+
+        // Assert -- Testergebnis überprüfen
+        Assert.Equal(25, term.Number1);
+        Assert.Equal(13, term.Number2);
+        Assert.Equal(CalcOperation.Addition, term.Operation);
+    }
+
+    [Theory]
+    [InlineData("25+13", 38)]
+    [InlineData("25-13", 12)]
+    [InlineData("25*13", 325)]
+    [InlineData("26/13", 2)]
+    public void Calculate_FundamentalOperations_ShouldReturnCorrectResult(string input, int expectedOutput)
+    {
+        // Arrange
+        var term = new Term(input).Parse();
+
+        // Act
+        var result = term.Calculate();
+
+        // Assert
+        Assert.Equal(expectedOutput, result);
+    }
+
+    // Bad Cases
+    [Fact]
+    public void Term_NullInput_ShouldThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Term(null));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Term_EmptyInput_ShouldThrowArgumentException(string input)
+    {
+        Assert.Throws<ArgumentException>(() => new Term(input));
+    }
+
+    [Theory]
+    [InlineData("12x34")]
+    public void Term_InvalidOperation_ShouldThrowInvalidOperationException(string input)
+    {
+        // Arrange
+        var term = new Term(input);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(term.Parse);
+    }
+
+    [Theory]
+    [InlineData("abc+123")]
+    public void Term_InvalidInput_ShouldThrowFormatException(string input)
+    {
+        // Arrange
+        var term = new Term(input);
+
+        // Act & Assert
+        Assert.Throws<FormatException>(term.Parse);
+    }
+}
---
+++ TermCalculator/Refactored/Term.cs
@@ -0,0 +1,70 @@
+﻿namespace TermCalculator.Refactored;
+
+public class Term
+{
+    public string Input { get; set; }
+    public double Number1 { get; set; }
+    public double Number2 { get; set; }
+    public CalcOperation Operation { get; set; }
+
+    public Term(string? input)
+    {
+        Input = input ?? throw new ArgumentNullException(nameof(input));
+        if (string.IsNullOrEmpty(Input.Trim()))
+        {
+            throw new ArgumentException(nameof(input), "Input cannot be empty");
+        }
+    }
+
+    public Term Parse()
+    {
+        Operation = GetCalcOperation();
+
+        string[] numbers = SplitTerm();
+
+        Number1 = double.Parse(numbers[0]);
+        Number2 = double.Parse(numbers[1]);
+
+        return this;
+    }
+
+    public double Calculate() => Operation switch
+    {
+        CalcOperation.Addition => Number1 + Number2,
+        CalcOperation.Subtraction => Number1 - Number2,
+        CalcOperation.Multiplication => Number1 * Number2,
+        CalcOperation.Division => Number1 / Number2,
+        _ => throw new InvalidOperationException("Invalid operation"),
+    };
+
+    private CalcOperation GetCalcOperation()
+    {
+        if (Input.Contains('+'))
+            return CalcOperation.Addition;
+        else if (Input.Contains('-'))
+            return CalcOperation.Subtraction;
+        else if (Input.Contains('*'))
+            return CalcOperation.Multiplication;
+        else if (Input.Contains('/'))
+            return CalcOperation.Division;
+        else
+            throw new InvalidOperationException("Invalid operation");
+    }
+
+    private string[] SplitTerm()
+    {
+        char separator = Operation switch
+        {
+            CalcOperation.Addition => '+',
+            CalcOperation.Subtraction => '-',
+            CalcOperation.Multiplication => '*',
+            CalcOperation.Division => '/',
+            _ => throw new InvalidOperationException("Invalid operation"),
+        };
+
+        var result = Input.Split(separator);
+        return result.Length == 2 
+            ? result 
+            : throw new FormatException("Invalid term format");
+    }
+}
